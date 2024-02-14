be00e80387ef821d6293e20b76a4786d6a1ba2d9
mazzlookman <mohammadlukmanaqib@gmail.com>
2024-02-14 21:55:22 +0700

    create validation errors

--- src/middleware/error-middleware.js
+++ src/middleware/error-middleware.js
@@ -1,4 +1,6 @@
 import {ResponseError} from "../exception/response-error.js";
+import joi from "joi";
+const {ValidationError} = joi;
 
 const errorMiddleware = async (err, req, res, next) => {
     if (!err) {
@@ -12,12 +14,31 @@ const errorMiddleware = async (err, req, res, next) => {
             status: err.statusText,
             errors: err.message
         }).end();
-    }  else {
+    }
+
+    else if (err instanceof ValidationError) {
+        const errors = [];
+        err.details.map((v) => {
+            const split = v.message.split(`"`);
+            errors.push(
+                {
+                    message: `${split[1]}${split[2]}`,
+                    type: v.type
+                });
+        });
+        res.status(400).json({
+            code: 400,
+            status: "Bad Request",
+            errors: errors
+        }).end();
+    }
+
+    else {
         console.log(err)
         res.status(500).json({
             code: err.statusCode,
             status: err.statusText,
-            errors: err.message
+            errors: err
         }).end();
     }
 }
--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,12 +1,10 @@
-import {ResponseError} from "../exception/response-error.js";
-
 export const validate = (schema, request) => {
     const result = schema.validate(request, {
         abortEarly: false,
     });
 
     if (result.error) {
-        throw new ResponseError(400, "Bad Request", result.error.message);
+        throw result.error;
     } else {
         return result.value;
     }
